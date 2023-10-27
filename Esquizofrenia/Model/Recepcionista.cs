1a713b3dda8d19ac828e102aaad4bba6ce4ab6d7
TheMFYisasCrist <jesusdilorenzo335@gmail.com>
2023-10-27 12:01:32 -0300

    first commit

---
+++ Esquizofrenia/Model/Recepcionista.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquizofrenia
+{
+    internal class Recepcionista
+    {
+        public string nombreApellido { get; set; }
+        public string dni { get; set; }
+
+        public string user { get; set; }
+        public string password { get; set; }
+
+        public void moviendomesa()
+        {
+
+        }
+    }
+}
+
